fbcc956599cfc9a73e6b49d135f6ba73640503bb
Diece98 <esther.mehal@gmail.com>
2023-11-23 22:18:45 +0100

    bug fix timer

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -10,34 +10,38 @@ document.addEventListener('DOMContentLoaded', function () {
     let interval;
 
     function start() {
-    startTime = Date.now();
-    interval = setInterval(updateDisplay, 10);
+        // Check if the timer is already running
+        if (!interval) {
+            startTime = Date.now();
+            interval = setInterval(updateDisplay, 10);
+        }
     }
 
     function stop() {
-    clearInterval(interval);
+        clearInterval(interval);
+        interval = null; // Reset the interval variable
     }
 
     function reset() {
-    clearInterval(interval);
-    minutes.textContent = '00';
-    seconds.textContent = '00';
-    milliseconds.textContent = '00';
+        clearInterval(interval);
+        interval = null; // Reset the interval variable
+        minutes.textContent = '00';
+        seconds.textContent = '00';
+        milliseconds.textContent = '00';
     }
 
     function updateDisplay() {
-    const elapsedTime = Date.now() - startTime;
-    const minutesValue = Math.floor(elapsedTime / 60000);
-    const secondsValue = Math.floor((elapsedTime - (minutesValue * 60000)) / 1000);
-    const millisecondsValue = Math.floor((elapsedTime - (minutesValue * 60000) - (secondsValue * 1000)) / 10);
+        const elapsedTime = Date.now() - startTime;
+        const minutesValue = Math.floor(elapsedTime / 60000);
+        const secondsValue = Math.floor((elapsedTime - (minutesValue * 60000)) / 1000);
+        const millisecondsValue = Math.floor((elapsedTime - (minutesValue * 60000) - (secondsValue * 1000)) / 10);
 
-    minutes.textContent = minutesValue < 10 ? '0' + minutesValue : minutesValue;
-    seconds.textContent = secondsValue < 10 ? '0' + secondsValue : secondsValue;
-    milliseconds.textContent = millisecondsValue < 10 ? '0' + millisecondsValue : millisecondsValue;
+        minutes.textContent = minutesValue < 10 ? '0' + minutesValue : minutesValue;
+        seconds.textContent = secondsValue < 10 ? '0' + secondsValue : secondsValue;
+        milliseconds.textContent = millisecondsValue < 10 ? '0' + millisecondsValue : millisecondsValue;
     }
 
     startBtn.addEventListener('click', start);
     stopBtn.addEventListener('click', stop);
     resetBtn.addEventListener('click', reset);
-
 });
